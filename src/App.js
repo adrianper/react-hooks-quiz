279f34fd4b164b5eabedc8acfbd9d66831d9c5f5
adrian <adrianperezm.00@gmail.com>
2021-07-27 18:01:35 -0600

    Examen React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Counter from "./Components/Counter";
 
-function App() {
+const styles = {
+  container: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    boxSizing: 'border-box'
+  }
+}
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <Router>
+        <Switch>
+          <Route path='/' exact>
+            <Counter />
+          </Route>
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
